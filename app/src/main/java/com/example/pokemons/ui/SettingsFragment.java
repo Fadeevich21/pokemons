1e426af2eec3fc50f360787b9707d7191407b778
Fadeevich21 <fad.danila053@gmail.com>
2023-03-20 12:55:50 +0300

    Вынес все размеры в файл dimens.xml

--- app/src/main/java/com/example/pokemons/ui/SettingsFragment.java
+++ app/src/main/java/com/example/pokemons/ui/SettingsFragment.java
@@ -1,28 +1,46 @@
 package com.example.pokemons.ui;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatDelegate;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.RadioGroup;
 
 import com.example.pokemons.R;
 
 public class SettingsFragment extends Fragment {
-    Button changeThemeButton;
+
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
     }
 
+    @SuppressLint("NonConstantResourceId")
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_settings, container, false);
+        View view = inflater.inflate(R.layout.fragment_settings, container, false);
+        RadioGroup radioGroup = view.findViewById(R.id.idRGgroup);
+        radioGroup.setOnCheckedChangeListener((group, checkedId) -> {
+            switch (checkedId) {
+                case R.id.idRBLight:
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                    break;
+
+                case R.id.idRBDark:
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                    break;
+            }
+        });
+
+        return view;
     }
 }
